db281c1ba540881a71a0269d8cf1f01a68beee31
Nikita Gupta <gnikita432@gmail.com>
2021-09-07 12:23:00 +0530

    leetcode- 1389

---
+++ Arrays and ArrayLists/9. Create Target Array in the Given Order.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+
+     int[] target = new int[nums.length];
+     int i = 0, k = 0;
+	 while (i < index.length) {
+		for (k = target.length - 1; k > index[i]; k--)
+			target[k] = target[k - 1];
+		target[index[i]] = nums[i];
+		i++;
+	}
+    return target;
+    }
+}
