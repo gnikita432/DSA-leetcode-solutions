c8921c29f8d2575bff1d9239762a679b025405ba
Nikita Gupta <gnikita432@gmail.com>
2021-09-20 21:34:02 +0530

    Find Peak Element

---
+++ Searching/Medium/1. Find Peak Element.java
@@ -0,0 +1,22 @@
+// 162. Find Peak Element
+
+class Solution {
+  public int findPeakElement(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+
+      if (arr[mid] > arr[mid + 1]) {
+        end = mid;
+      }
+      else {
+        start = mid + 1;
+      }
+    }
+
+    return start;
+  }
+}
+// Time Complexity : O(log n)
