5d88c30536c9fb970e71e68d41ef91fa5d6e69e5
Nikita Gupta <gnikita432@gmail.com>
2021-09-21 16:00:09 +0530

    leetcode- 33 (duplicate numbers case added)

--- Searching/Medium/2. Search in Rotated Sorted Array.java
+++ Searching/Medium/2. Search in Rotated Sorted Array.java
@@ -42,6 +42,44 @@ class Solution {
     return -1;
   }
 
+  //finding pivot with duplicate numbers
+  public int findPivotWithDuplicates(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+    
+        if (mid < end && arr[mid] > arr[mid + 1]) {
+        return mid;
+        }
+        if (mid > start && arr[mid] < arr[mid - 1]) {
+        return mid - 1;
+        }
+
+        if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+        
+        if (start < end && arr[start] > arr[start + 1]) {
+            return start;
+        }
+        start++;
+
+        if (end > start && arr[end] < arr[end - 1]) {
+            return end - 1;
+        }
+        end--;
+        }
+        // left side is sorted, so pivot should be in right
+        else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+        start = mid + 1;
+        } else {
+        end = mid - 1;
+        }
+    }
+    return -1;
+    }
+
+
      //Binary Search
   public int binarySearch(int[] nums, int target, int start, int end) {
 
