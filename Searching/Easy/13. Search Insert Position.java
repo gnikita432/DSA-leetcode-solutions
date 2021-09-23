b7eadc391773c7dda13e82a45a39b72f551eedef
Nikita Gupta <gnikita432@gmail.com>
2021-09-23 11:31:12 +0530

    leetcode- 35

---
+++ Searching/Easy/13. Search Insert Position.java
@@ -0,0 +1,24 @@
+// 35. Search Insert Position
+
+//Binary Search Approach
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int start = 0, end = nums.length-1;
+
+        while(start<=end){
+          int mid = start + (end - start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }
+            else{
+                end= mid-1;
+            }
+        }
+        return start;
+    }
+}
+// Time Complexity : O(log n)
