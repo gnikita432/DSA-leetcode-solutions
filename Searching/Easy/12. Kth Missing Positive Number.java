e06b14b48eca1eb65823aa213eebabaf5c63ed7d
Nikita Gupta <gnikita432@gmail.com>
2021-09-23 11:00:10 +0530

    leetcode- 1539 binary search approach

--- Searching/Easy/12. Kth Missing Positive Number.java
+++ Searching/Easy/12. Kth Missing Positive Number.java
@@ -19,3 +19,49 @@ class Solution {
     }
 }
 // Time Complexity : O(n)
+
+
+//Approach 2 Binary Search- O(log n)
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+
+           int start=0, end=arr.length-1;
+     
+        while(start<=end){
+            
+            int mid=start+(end-start)/2;
+            
+            if(arr[mid]-mid-1<k){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    return k+start; // arr[end] + (k-missingnumberscount)
+    }
+}
+// Time Complexity : O(log n)
+
+
+  //Approach 3
+  //By freqquency Array
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int[] count = new int[1001];
+        for(int n : arr){
+            count[n]++;
+        }
+        
+        int c = 1, ans=0;
+        while(ans<k){
+            if (c>=arr.length||count[c] == 0){
+                ans++;
+            }
+            c++;
+        }
+        return c-1;
+    }
+}
+ // Not working for some test cases
+ 
