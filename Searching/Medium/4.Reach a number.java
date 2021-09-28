11ada18d6d77c7ee7a8a3eede9f25b507e560b88
Nikita Gupta <gnikita432@gmail.com>
2021-09-28 18:34:46 +0530

    leetcode- 754

---
+++ Searching/Medium/4.Reach a number.java
@@ -0,0 +1,21 @@
+// 754. Reach a number
+
+class Solution {
+    public int reachNumber(int target) {
+        
+        target = Math.abs(target);
+        int step = 0;
+        int sum = 0;
+        
+        while (sum < target) {
+            step++;
+            sum += step;
+        }
+        while ((sum - target) % 2 != 0) {
+            step++;
+            sum += step;
+        }
+        return step;
+    }
+}
+// Time Complexity : O(n)
