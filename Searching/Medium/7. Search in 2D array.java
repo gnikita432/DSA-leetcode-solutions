19f9b8fa47990ef90a1b28cbf4c781427e346031
Nikita Gupta <gnikita432@gmail.com>
2021-10-02 16:13:55 +0530

    leetcode- 74

--- Searching/Medium/7. Search in 2D array.java
+++ Searching/Medium/7. Search in 2D array.java
@@ -46,7 +46,7 @@ class Solution {
 
 
 //Approach 3
-//If matrix if sorted by row wise and by column wise so we can use this binary search
+//Row Column Matrix- If matrix if sorted by row wise and by column wise so we can use this binary search
 //but this method is not optimum if matrix is sorted like Integers in each row are sorted from left to right and The first integer of each row is greater than the last integer of the previous row.
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
@@ -71,4 +71,78 @@ class Solution {
 
 
 //Approach 4 
-//Optial binary search approach for matix if  matrix is sorted like Integers in each row are sorted from left to right and The first integer of each row is greater than the last integer of the previous row.
+//Strictly Sorted Matrix- Optial binary search approach for matix if  matrix is sorted like Integers in each row are sorted from left to right and The first integer of each row is greater than the last integer of the previous row.
+
+//Take middle column
+//Perform binary search on this middle column to eliminate rows - O(log n)
+
+//if element == target {return ans}
+//else if element > target {ignore all the rows after that elements row}
+//else if element < target {ignore all the rows above that elements row}
+
+//In the last 2 rows will be remaining
+//Check whether the mid column you are at contains the ans?
+//if not then consider four parts- [i][j-1]   | [i][j] | [i][j+1]
+//                                 [i+1][j-1] |[i+1][j]| [i+1][j+1]   
+//now do simple binary search in this 4 parts - O(log m)
+//Time Complexity - O(log n + log m)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int rstart = 0, rend = matrix.length-1, cstart = 0, cend = matrix[0].length-1;
+        
+        int cmid = cstart + (cend-cstart)/2;    
+     
+        while(rstart < (rend - 1)){
+        int rmid = rstart + (rend-rstart)/2;
+            
+            if(matrix[rmid][cmid]==target){
+                return true;
+            }
+            else if(matrix[rmid][cmid]>target){
+                rend = rmid;
+            }
+            else{
+                rstart = rmid;
+            }
+        }
+        
+        if(matrix[rstart][cmid]==target){
+            return true;
+        }
+        else if(matrix[rend][cmid]==target){
+            return true;
+        }
+        else{
+            if(binarysearch(matrix, rstart, 0, cmid-1, target))
+                return true;
+            else if(binarysearch(matrix, rend, 0, cmid-1, target))
+                return true;
+            else if(binarysearch(matrix, rstart, cmid+1, matrix[0].length-1, target))
+                return true;
+            else if(binarysearch(matrix, rend, cmid+1, matrix[0].length-1, target))
+                return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean binarysearch(int[][]matrix, int row, int start, int end, int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(matrix[row][mid]==target){
+                return true;
+            }
+            else if(matrix[row][mid]>target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+}
+//Time Complexity : O(log n)
+
+//Approach 5 convert it in 1D array then apply binary search - not optimal solution
