d0930cecd4eb1ec89b2e399655bf7d0fc11c971b
Nikita Gupta <gnikita432@gmail.com>
2021-10-04 16:28:21 +0530

    leetcode- 4 binary search approach

--- Searching/Hard/3. Median of Two Sorted Arrays.java
+++ Searching/Hard/3. Median of Two Sorted Arrays.java
@@ -100,3 +100,42 @@ class Solution {
 }
 // Time Complexity : O(n+m)
 // Space Complexity : O(1);
+
+
+//Binary Search Approach - Most Optimal Approach
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        if(nums2.length<nums1.length)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int low = 0, high = nums1.length;
+        
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int cut1 = mid;
+            int cut2 = (nums1.length + nums2.length + 1)/2 - cut1;
+            
+            int l1 = cut1==0? Integer.MIN_VALUE : nums1[cut1-1]; 
+            int l2 = cut2==0? Integer.MIN_VALUE : nums2[cut2-1];
+            int r1 = cut1==nums1.length? Integer.MAX_VALUE : nums1[cut1]; 
+            int r2 = cut2==nums2.length? Integer.MAX_VALUE : nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1){
+                if((nums1.length+nums2.length)%2==0)
+                    return (Math.max(l1,l2)+Math.min(r1,r2)) / 2.0;
+                else
+                    return Math.max(l1,l2) ;
+            }
+            else if(l1>r2){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return 0.0;
+    }
+}
+// Time Complexity : O(log min(n,m))
+// Space Complexity : O(1)
