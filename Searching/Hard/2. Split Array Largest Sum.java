f5be51e8c3b9d8c4ad73505343d65e5405b51344
Nikita Gupta <gnikita432@gmail.com>
2021-10-06 08:16:00 +0530

    leetcode- 410

--- Searching/Hard/2. Split Array Largest Sum.java
+++ Searching/Hard/2. Split Array Largest Sum.java
@@ -1,5 +1,64 @@
 // 410. Split Array Largest Sum
 
+//Binary search approach with explanation
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        
+       // if I split the array in exactly 1 way (minimum) then only 1 comibination can be created that is the whole array  | 7,2,5,10,8 whith m = 1 piece so sum af all the elements will eventully my ans and as no other comibations are possible so the whole sum would be the onlly ans as max sum would be sum off all pices(piece = 1) ie sum of whole array
+        //ie the max ans possible is sum of whole array
+       
+            
+        //Now if i split array to its maximum that is nums.length-1, as array can be split beyoud this, this will be invalid as all elements couldnt be fixed.
+        //so if array splits in nums.length-1 ie m = 5-1 = 4 then only 1 combination is possible like the previous one, the combination is
+        // 7| 2 | 5 | 10 | 8 and max sum from all the splits is 10 ie ans=10 for max splits
+        
+         int sum = 0, max=nums[0];
+        for(int num : nums){
+            sum+=num;
+            if(num>max)
+                max=num;
+        }
+        
+        // Now the lower bound becomes 10 and uper bound becomes whole sum ie 32
+        //all splits can be between 1 and 32 so ans can be between this only
+        
+        int start = max, end = sum;
+        
+        while(start<end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(ismpieces(nums, m, mid)){
+                end = mid; // As we want the smallest so we dont return it but we will check more in the left side for sum minimum value and if they ask us for max so we would do this with start.
+            }
+            else{
+                start = mid + 1; //as piceses will be more thn m so our min value is less we need to check for a greater value
+            }
+        }
+        return start; //or end
+    }
+    
+    public boolean ismpieces(int[] nums, int m, int mid){
+        int sum = 0, pices=1;
+        for(int num : nums){
+            if(sum+num > mid){
+                pices++;
+                sum = num;
+            }
+            else
+                sum+=num;
+            
+        }
+        if(pices <= m){
+            return true;
+        }
+        return false;
+    }
+}
+// Time Complexity : O(n log n)
+
+
+
 //Binary Search Approach
 class Solution {
   public int splitArray(int[] nums, int m) {
--- Searching/Questions.md
+++ Searching/Questions.md
@@ -37,6 +37,7 @@
 
 - [Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/)
 - [Find Minimum in Rotated Sorted Array II](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/)
+- [Split Array Largest Sum](https://leetcode.com/problems/split-array-largest-sum/)
 
 # UnSolved Questions
 
@@ -59,7 +60,6 @@
 
 - [Aggressive cows](https://www.spoj.com/problems/AGGRCOW/)
 - [Book allocation](https://www.geeksforgeeks.org/allocate-minimum-number-pages/)
-- [Split Array Largest Sum](https://leetcode.com/problems/split-array-largest-sum/)
 - [Find in Mountain Array](https://leetcode.com/problems/find-in-mountain-array/)
 - [Count smaller number after Self](https://leetcode.com/problems/count-of-smaller-numbers-after-self/)
 - [Divide Chocolate Problem](https://curiouschild.github.io/leetcode/2019/06/21/divide-chocolate.html)
@@ -71,3 +71,4 @@
 - [Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/)
 - [Search in Rotated Sorted Array II](https://leetcode.com/problems/search-in-rotated-sorted-array-ii/)
 - [Find Minimum in Rotated Sorted Array II](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/)
+- [Split Array Largest Sum](https://leetcode.com/problems/split-array-largest-sum/)
