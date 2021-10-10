9959cb8e762635724f3b748109022734f4a7f993
Nikita Gupta <gnikita432@gmail.com>
2021-10-10 19:03:19 +0530

    Cyclic Sort

---
+++ Sorting/04. Cyclic Sort.java
@@ -0,0 +1,31 @@
+// Cyclic Sort
+
+//Used when numbers of arrays are in the range 1 - N
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+// Time Complexity : O(n-1 + n) = O(n)
+// Best case = average case = worst case = O(n)
+// Auxiliary Space : O(1)
