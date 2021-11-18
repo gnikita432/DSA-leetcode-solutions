e1b67741211401644589e596d948e460c52e24d5
Nikita Gupta <gnikita432@gmail.com>
2021-11-18 11:36:59 +0530

    Memoization approach

--- Dynamic Programming/Easy/2.Climb Stairs With Minimum Moves.java
+++ Dynamic Programming/Easy/2.Climb Stairs With Minimum Moves.java
@@ -20,4 +20,34 @@
         }
         return min + 1;
     }
+
+//DP - Memoization Approach
+
+ public static int minpaths(int idx, int n, int[] arr, int[] qb){
+        
+        if(idx == n){
+            return 0;
+        }
+        if(qb[idx]>0){
+            return qb[idx];
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int jumps=1; jumps<=arr[idx] && jumps+idx<=n; jumps++){
+            int path = minpaths(idx+jumps, n, arr, qb);
+           if(min > path){
+               min = path;
+           }
+        }
+        
+        int ans = min + 1;
+        
+        if(min==Integer.MAX_VALUE){
+            ans = min; //returning min because it will be the maximum among all the other path while if i return min+1 in this case it will exceed the range and the value will go negative and therefore minimum answer will come as this min value -23..... 
+        }
+       
+        qb[idx] = ans;
+        
+        return ans; 
+    }
     
