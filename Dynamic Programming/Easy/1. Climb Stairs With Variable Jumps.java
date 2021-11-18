15b6de9a3fbbfaea54a2c44e60f16f4eac9bc01b
Nikita Gupta <gnikita432@gmail.com>
2021-11-18 09:20:11 +0530

    through memoization

--- Dynamic Programming/Easy/1. Climb Stairs With Variable Jumps.java
+++ Dynamic Programming/Easy/1. Climb Stairs With Variable Jumps.java
@@ -17,4 +17,25 @@ public static int countPaths(int[] arr, int index, int len) {
     return count;
   }
 
+  //DP - Memoization approach
+
+public static int paths(int n, int[] arr, int idx, int[] qb){
+      
+      if(idx==n){
+          return 1;
+      }
+      if(qb[idx]>0){
+          return qb[idx];
+      }
+      
+      int path = 0;
+      for(int i = 1 ; i<=arr[idx]; i++){
+          if(i+idx <= n){
+      path += paths(n, arr, i+idx, qb);
+          }
+      }
+      
+      qb[idx] = path;
+      return path;
+  }
   
