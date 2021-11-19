89c23fe8a1efe1723eacf098027a0ac8f17c7c64
Nikita Gupta <gnikita432@gmail.com>
2021-11-19 08:37:17 +0530

    by dp - memoization

--- Dynamic Programming/Medium/2 Combination Sum IV (Permutations of coins).java
+++ Dynamic Programming/Medium/2 Combination Sum IV (Permutations of coins).java
@@ -27,3 +27,32 @@ class Solution {
     }
 }
 
+
+// 2 Dp - memoization Approach - 100% faster
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        return permutations(nums, target, new Integer[target+1]);
+        
+    }
+    
+    public int permutations(int[] nums, int target, Integer[] qb){
+        
+        if(target == 0){
+            return 1;
+        }
+        if(target<0){
+            return 0;
+        }
+        if(qb[target]!=null){
+            return qb[target];
+        }
+        int count = 0 ;
+        for(int i=0; i<nums.length; i++){
+           count+= permutations(nums, target-nums[i], qb);
+        }
+        return qb[target] = count;
+    }
+}
+
