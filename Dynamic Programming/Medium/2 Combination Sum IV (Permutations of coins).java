0fa0d6ad3dfa0ee69c4f66596b9fb4eda96cf61a
Nikita Gupta <gnikita432@gmail.com>
2021-11-19 09:44:04 +0530

    with tabulation approach

--- Dynamic Programming/Medium/2 Combination Sum IV (Permutations of coins).java
+++ Dynamic Programming/Medium/2 Combination Sum IV (Permutations of coins).java
@@ -56,3 +56,23 @@ class Solution {
     }
 }
 
+
+// 3 Dp - Tabulation Approach
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        // return permutations(nums, target, new Integer[target+1]);
+        
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        
+        for(int c=1; c<dp.length; c++){
+            for(int n=0; n<nums.length; n++){
+                if(c-nums[n]>=0){
+                dp[c]+=dp[c-nums[n]];
+                }
+            }
+        }
+        return dp[target];
+    }
