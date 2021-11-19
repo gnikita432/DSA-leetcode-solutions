26a10e5ab8864693bbf2df031605e920b8304797
Nikita Gupta <gnikita432@gmail.com>
2021-11-19 14:28:36 +0530

    by recurssion

---
+++ Dynamic Programming/Medium/3 zero one knapsack.java
@@ -0,0 +1,28 @@
+//Zero One knapsack
+//Similar to target sum subset
+
+//https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+
+//Approach 1 - Recurssion
+ static int knapSack(int W, int wt[], int val[], int n)
+    {
+        // Base Case
+        if (n == 0 || W == 0)
+            return 0;
+ 
+        // If weight of the nth item is
+        // more than Knapsack capacity W,
+        // then this item cannot be included
+        // in the optimal solution
+        if (wt[n - 1] > W)
+            return knapSack(W, wt, val, n - 1);
+ 
+        // Return the maximum of two cases:
+        // (1) nth item included
+        // (2) not included
+        else
+            return Math.max(val[n - 1]
+                       + knapSack(W - wt[n - 1], wt,
+                                  val, n - 1),
+                       knapSack(W, wt, val, n - 1));
+    }
