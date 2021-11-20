11ec57dc26afabf7c9cfe929625f9938fa05ef06
Nikita Gupta <gnikita432@gmail.com>
2021-11-20 12:27:34 +0530

    Arrange buildings tabulation

---
+++ Dynamic Programming/Easy/7. Arrange Buildings.java
@@ -0,0 +1,29 @@
+// Arrange Buildings
+
+// Dp Tabulation Approach
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    Scanner scn = new Scanner(System.in);
+    long n = scn.nextInt();
+
+    int prevbuilding = 1;
+    int prevspace = 1;
+
+    for (int i = 2; i <= n; i++) {
+      int newbuilding = prevspace;
+      int newspace = prevspace + prevbuilding;
+
+      prevspace = newspace;
+      prevbuilding = newbuilding;
+    }
+    long total = prevspace + prevbuilding;
+    System.out.println(total * total);
+  }
+
+}
