4b8a665d629134a96a7830d9dcd3e75aad96d495
Nikita Gupta <gnikita432@gmail.com>
2021-12-09 21:39:52 +0530

    Pre order traversal

---
+++ Binary Tree/Pre In Post Order Traversal.java
@@ -0,0 +1,39 @@
+// Pre Post In Order Traversal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ // Pre Order Traversal
+
+class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List <Integer> ans = new ArrayList<>();
+        return preorder(ans, root);
+        
+    }
+    public List<Integer> preorder(List ans , TreeNode root){
+        if(root == null){
+            return ans;
+        }
+        
+        ans.add(root.val);
+        preorder(ans, root.left);
+        preorder(ans, root.right);
+        
+        return ans;
+    }
+}
