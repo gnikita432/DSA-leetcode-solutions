f3daefae322f734fe8785af665e21cd2d23d35d3
Nikita Gupta <gnikita432@gmail.com>
2021-12-19 21:30:49 +0530

    tilt of binary tree

---
+++ Binary Tree/8. tilt of binary tree.java
@@ -0,0 +1,22 @@
+// 563. Binary Tree Tilt
+
+public class Solution {
+    int result = 0;
+    
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+    
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+        
+        result += Math.abs(left - right);
+        
+        return left + right + root.val;
+        
+    }
+}
