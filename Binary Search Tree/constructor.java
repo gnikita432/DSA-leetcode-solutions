52a1d2cf8782dc4fa4b727618dd2686daceddd11
Nikita Gupta <gnikita432@gmail.com>
2022-01-17 13:29:19 +0530

    bst constructor

---
+++ Binary Search Tree/constructor.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  
+  public static Node construct(Integer[] arr, int lo, int hi) {
+    
+    if(lo>hi){
+        return null;
+    }
+
+    int mid = (hi + lo) / 2;
+
+    int data = arr[mid];
+
+    Node lc = construct(arr, lo, mid-1);
+    Node rc = construct(arr, mid+1, hi);
+
+    Node node = new Node(data, lc, rc);
+
+    return node;
+
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr, 0, arr.length-1);
+
+  }
+
+}
