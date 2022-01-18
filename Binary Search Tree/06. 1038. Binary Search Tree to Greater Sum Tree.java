3ce371af832efc45b7c560ea7506969f8d3f5429
Nikita Gupta <gnikita432@gmail.com>
2022-01-18 12:06:08 +0530

    bst to gst

---
+++ Binary Search Tree/06. 1038. Binary Search Tree to Greater Sum Tree.java
@@ -0,0 +1,21 @@
+// 1038. Binary Search Tree to Greater Sum Tree
+
+class Solution {
+    
+   int sum = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {
+        
+        if(root == null){
+            return null;
+        }
+        
+        bstToGst(root.right);
+         sum+=root.val;
+         root.val = sum;
+        bstToGst(root.left);
+        
+        return root;
+        
+    }
+}
